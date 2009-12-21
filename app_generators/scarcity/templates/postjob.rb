fab43e6d01026dd0d92f4db030eb00b2627d8786
Kristopher Kosmatka <kjkosmatka@gmail.com>
2009-12-21 10:43:40 -0600

    removing old ruby default scripts

--- app_generators/scarcity/templates/postjob.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-# This is the POST script that is run after each node in the DAG
-# Note this is run on the submit machine, in the run directory for
-# the particular dataset.
-
--- app_generators/scarcity/templates/prejob.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# This is the PRE script that is run before each node in the DAG
-# Note that this runs on the submit machine, in the run directory for
-# the particular dataset
